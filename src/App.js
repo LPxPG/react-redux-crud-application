ddc78c947bbd0cfc97d1f9d11f96f2cd774ceb59
gitkuro <zebragel-cobaltblue@yahoo.co.jp>
2021-01-14 21:04:50 +0900

    learn state and setState

--- src/App.js
+++ src/App.js
@@ -1,48 +1,33 @@
-import React from 'react'; // React : JSX使用時は要import
-import PropTypes from 'prop-types'
+import React, {Component} from 'react'; // React : JSX使用時は要import
 
-const App = () => {
-  const profiles = [
-    {
-      name: "Taro",
-      age : 10
-    },
-    {
-      name: "Hanako",
-      age :5
-    },
-    {
-      name: "NoName",
-    },
-    {
-      name: 1, // 型定義エラー
-      age : "5" // 型定義エラー
-    }
-  ]
-  return (
-    <div>
-    {
-      profiles.map((profile, index) => {
-        return  <User name={profile.name} age={profile.age} key={index} />
-      })
-      // key : uniqueな値をもつpropが必要
-    }
-      {/*
-      <User name={"Taro"} age={10} />
-      <User name={"Hanako"} age={5} />
-      */}
-    </div>
-  )
+const App = () => (<Counter></Counter>)
+
+class Counter extends Component {
+  constructor(props) {
+    super(props)
+    this.state = { count : 0}
+
+  }
+
+  handlePlusButton = () => {
+    this.setState({ count: this.state.count + 1, name : "test"})
+  }
+  handleMinusButton = () => {
+    this.setState({ count: this.state.count - 1})
+  }
+
+  render() {
+    return (
+      <React.Fragment>
+        <div>count : {this.state.count} </div>
+        <div>name : {this.state.name} </div> {/* 後から追加できる模様 */}
+        <button onClick={this.handlePlusButton}>+1</button>
+        <button onClick={this.handleMinusButton}>-1</button>
+      </React.Fragment>
+    )
+  }
 }
 
-const User = (props) => {
-  return <div>Hi, I am {props.name}, and {props.age} years old!</div>
-}
-
-// プロパティの型定義
-User.propTypes = {
-  name: PropTypes.string,
-  age: PropTypes.number.isRequired, // isRequired : 必須プロパティ
-}
+// state : クラスコンポーネントのみで使用可
 
 export default App;
