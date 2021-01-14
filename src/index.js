afc6b180f02ef98d09719dd6fc411eae9fece4f0
gitkuro <zebragel-cobaltblue@yahoo.co.jp>
2021-01-14 22:03:14 +0900

    implement store

--- src/index.js
+++ src/index.js
@@ -1,10 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore } from "redux"
+import { Provider } from "react-redux"
+
 import './index.css';
-import App from './App';
+import reducer from "./reducers"
+
+import App from './components/App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+// store にstate が集約される
+const store = createStore(reducer)
+
+
+// <App /> を <Provider>でラップするとアプリケーション内の全階層のコンポーネントからstoreが利用可能になる。
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  // <App />,
+  document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
