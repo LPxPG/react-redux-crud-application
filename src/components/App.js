af8f3d1c60029a8ad5b03ee60073a9535a329419
gitkuro <zebragel-cobaltblue@yahoo.co.jp>
2021-01-14 22:22:30 +0900

    Implement component with connect function

--- src/components/App.js
+++ src/components/App.js
@@ -1,33 +1,47 @@
-import React, {Component} from 'react'; // React : JSX使用時は要import
+import React, {Component} from 'react'
+import { connect } from "react-redux"
 
-const App = () => (<Counter></Counter>)
+import { increment, decrement } from "../actions"
 
-class Counter extends Component {
-  constructor(props) {
-    super(props)
-    this.state = { count : 0}
+class App extends Component {
+  // constructor(props) {
+  //   super(props)
+  //   this.state = { count : 0}
+  // }
+  // state の初期化をreducer で行うため不要に。
 
-  }
-
-  handlePlusButton = () => {
-    this.setState({ count: this.state.count + 1, name : "test"})
-  }
-  handleMinusButton = () => {
-    this.setState({ count: this.state.count - 1})
-  }
+  // handlePlusButton = () => {
+  //   this.setState({ count: this.state.count + 1})
+  // }
+  // handleMinusButton = () => {
+  //   this.setState({ count: this.state.count - 1})
+  // }
+  // action creator, reducer で処理するため不要に。
 
   render() {
+    const props = this.props
+
     return (
       <React.Fragment>
-        <div>count : {this.state.count} </div>
-        <div>name : {this.state.name} </div> {/* 後から追加できる模様 */}
+        <div>value : { props.value } </div>
+        <button onClick={ props.increment }>+1</button>
+        <button onClick={ props.decrement }>-1</button>
+        {/* <div>count : {this.state.count} </div>
         <button onClick={this.handlePlusButton}>+1</button>
-        <button onClick={this.handleMinusButton}>-1</button>
+        <button onClick={this.handleMinusButton}>-1</button> */}
       </React.Fragment>
     )
   }
 }
 
-// state : クラスコンポーネントのみで使用可
+const mapStateToProps = state => ({value: state.count.value})
 
-export default App;
+// const mapDispatchToProps = dispatch => ({
+//   increment : () => dispatch(increment()),
+//   decrement : () => dispatch(decrement()),
+// })
+
+// 上記の省略記法
+const mapDispatchToProps = ({ increment, decrement })
+
+export default connect(mapStateToProps, mapDispatchToProps)(App)
