dcf4abcb3b0ba5d637dfda7c6e821186294af993
gitkuro <zebragel-cobaltblue@yahoo.co.jp>
2021-01-15 17:38:14 +0900

    implement events index

--- src/index.js
+++ src/index.js
@@ -1,23 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createStore } from "redux"
+import { createStore, applyMiddleware } from "redux"
 import { Provider } from "react-redux"
+import thunk from "redux-thunk" // 要 applyMiddleware
 
 import './index.css';
 import reducer from "./reducers"
 
-import App from './components/App';
+import EventsIndex from './components/events_index';
 import * as serviceWorker from './serviceWorker';
 
 
 // store にstate が集約される
-const store = createStore(reducer)
+const store = createStore(reducer, applyMiddleware(thunk))
 
 
 // <App /> を <Provider>でラップするとアプリケーション内の全階層のコンポーネントからstoreが利用可能になる。
 ReactDOM.render(
   <Provider store={store}>
-    <App />
+    <EventsIndex />
   </Provider>,
   // <App />,
   document.getElementById('root')
